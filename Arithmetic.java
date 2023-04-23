ce408be36b2d06d3168c3e91030daf12b85ab970
Senthilkumar <128904610+senthilusa4@users.noreply.github.com>
2023-04-23 19:34:43 +0400

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,9 @@
+class Arithmetic
+{
+public static void main(String args[])
+  {
+  int a=10,b=2;
+  int c=a*b;
+  System.out.println(c);
+  }
+}
---
+++ Billing.java
@@ -0,0 +1,24 @@
+class Billing
+  {
+  public static void main(String args[])
+    {
+    Billing obj = new Billing();
+    int total_amount = obj.addition();
+                       obj.discount(total_amount); 
+    }
+    public int addition()
+      {
+       int tomato = 40;
+       int onion = 120;
+       int total = tomato + onion;
+       System.out.println("tomato rate is: " + tomato);
+       System.out.println("onion rate is: " + onion);
+       System.out.println("total amount is: " + total);
+       return total;
+      }  
+      public void discount(int total)
+        {
+         int after_discount_amount = total - 5;
+         System.out.println("after a discount total_amount: " + after_discount_amount);       
+        } 
+   }
